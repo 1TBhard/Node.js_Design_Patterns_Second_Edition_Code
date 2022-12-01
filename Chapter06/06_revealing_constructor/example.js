d9a934522fda791f4c4a903c2b6fa21b2ab1dbd3
1TBhard <was1383@naver.com>
2022-12-01 13:34:34 +0900

    공개 생성자 패턴

--- Chapter06/06_revealing_constructor/example.js
+++ Chapter06/06_revealing_constructor/example.js
@@ -1,7 +1,10 @@
 "use strict";
 
-const ticker = require('./ticker');
+const ticker = require("./ticker");
 
-ticker.on('tick', (tickCount) => console.log(tickCount, 'TICK'));
+const cb = (tickCount) => console.log(tickCount, "TICK");
+
+// ticker.on("tick", cb);
 // ticker.emit('something', {}); <-- This will fail
 // require('events').prototype.emit.call(ticker, 'someEvent', {}); <-- This workaround will instead work
+require("events").prototype.emit.call(ticker, "someEvent", {});
--- Chapter06/06_revealing_constructor/roee.js
+++ Chapter06/06_revealing_constructor/roee.js
@@ -1,12 +1,13 @@
 "use strict";
 
-const EventEmitter = require('events');
+const EventEmitter = require("events");
 
 module.exports = class Roee extends EventEmitter {
-  constructor (executor) {
-    super();
-    const emit = this.emit.bind(this);
-    this.emit = undefined;
-    executor(emit);
-  }
+	constructor(executor) {
+		super();
+		const emit = this.emit.bind(this);
+		// Roee.emit() 사용 불가
+		this.emit = undefined;
+		executor(emit);
+	}
 };
