68ae637d8b361435c519ca0141e0c399481bbb70
1TBhard <was1383@naver.com>
2022-12-01 15:55:05 +0900

    전략 패턴

--- Chapter06/16_strategy/config.js
+++ Chapter06/16_strategy/config.js
@@ -1,31 +1,32 @@
 "use strict";
 
-const fs = require('fs');
-const objectPath = require('object-path');
+const fs = require("fs");
+const objectPath = require("object-path");
 
+// 전략 패턴의 context
 class Config {
-  constructor(strategy) {
-    this.data = {};
-    this.strategy = strategy;
-  }
+	constructor(strategy) {
+		this.data = {};
+		this.strategy = strategy;
+	}
 
-  get(path) {
-    return objectPath.get(this.data, path);
-  }
+	get(path) {
+		return objectPath.get(this.data, path);
+	}
 
-  set(path, value) {
-    return objectPath.set(this.data, path, value);
-  }
+	set(path, value) {
+		return objectPath.set(this.data, path, value);
+	}
 
-  read(file) {
-    console.log(`Deserializing from ${file}`);
-    this.data = this.strategy.deserialize(fs.readFileSync(file, 'utf-8'));
-  }
+	read(file) {
+		console.log(`Deserializing from ${file}`);
+		this.data = this.strategy.deserialize(fs.readFileSync(file, "utf-8"));
+	}
 
-  save(file) {
-    console.log(`Serializing to ${file}`);
-    fs.writeFileSync(file, this.strategy.serialize(this.data));
-  }
+	save(file) {
+		console.log(`Serializing to ${file}`);
+		fs.writeFileSync(file, this.strategy.serialize(this.data));
+	}
 }
 
 module.exports = Config;
--- Chapter06/16_strategy/strategies.js
+++ Chapter06/16_strategy/strategies.js
@@ -1,13 +1,15 @@
 "use strict";
 
-const ini = require('ini');
+const ini = require("ini");
 
+// 개별 전략
 module.exports.json = {
-  deserialize: data => JSON.parse(data),
-  serialize: data => JSON.stringify(data, null, '  ')
+	deserialize: (data) => JSON.parse(data),
+	serialize: (data) => JSON.stringify(data, null, "  "),
 };
 
+// 개별 전략
 module.exports.ini = {
-  deserialize: data => ini.parse(data),
-  serialize: data => ini.stringify(data)
+	deserialize: (data) => ini.parse(data),
+	serialize: (data) => ini.stringify(data),
 };
