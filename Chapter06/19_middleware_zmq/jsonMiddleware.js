fe7cbe051ddb43af679492ce0aa8c7e39a6ec3cd
1TBhard <was1383@naver.com>
2022-12-02 15:33:26 +0900

    middleware 패턴

--- Chapter06/19_middleware_zmq/jsonMiddleware.js
+++ Chapter06/19_middleware_zmq/jsonMiddleware.js
@@ -1,14 +1,16 @@
 "use strict";
 
 module.exports.json = () => {
-  return {
-    inbound: function (message, next) {
-      message.data = JSON.parse(message.data.toString());
-      next();
-    },
-    outbound: function (message, next) {
-      message.data = new Buffer(JSON.stringify(message.data));
-      next();
-    }
-  }
+	return {
+		inbound: function (message, next) {
+			// 역직렬화
+			message.data = JSON.parse(message.data.toString());
+			next();
+		},
+		outbound: function (message, next) {
+			// 직렬화
+			message.data = new Buffer(JSON.stringify(message.data));
+			next();
+		},
+	};
 };
--- Chapter06/19_middleware_zmq/zmqMiddlewareManager.js
+++ Chapter06/19_middleware_zmq/zmqMiddlewareManager.js
@@ -1,52 +1,52 @@
 "use strict";
 
-
 module.exports = class ZmqMiddlewareManager {
-  constructor(socket) {
-    this.socket = socket;
-    this.inboundMiddleware = [];           //[1]
-    this.outboundMiddleware = [];
-    socket.on('message', message => {       //[2]
-      this.executeMiddleware(this.inboundMiddleware, {
-        data: message
-      });
-    });
-  }
+	constructor(socket) {
+		this.socket = socket;
+		this.inboundMiddleware = [];
+		this.outboundMiddleware = [];
 
-  send(data) {
-    const message = {
-      data: data
-    };
+		socket.on("message", (message) => {
+			// message 이벤트에 대한 새로운 리스너 연결 및 새 메지시 수신시작
+			this.executeMiddleware(this.inboundMiddleware, {
+				data: message,
+			});
+		});
+	}
 
-    this.executeMiddleware(this.outboundMiddleware, message,
-      () => {
-        this.socket.send(message.data);
-      }
-    );
-  }
+	send(data) {
+		const message = {
+			data: data,
+		};
+		this.executeMiddleware(this.outboundMiddleware, message, () => {
+			this.socket.send(message.data);
+		});
+	}
 
-  use(middleware) {
-    if (middleware.inbound) {
-      this.inboundMiddleware.push(middleware.inbound);
-    }
-    if (middleware.outbound) {
-      this.outboundMiddleware.unshift(middleware.outbound);
-    }
-  }
+	use(middleware) {
+		if (middleware.inbound) {
+			// 목록의 끝으로 푸시
+			this.inboundMiddleware.push(middleware.inbound);
+		}
+		if (middleware.outbound) {
+			// 목록의 처음으로 푸시
+			this.outboundMiddleware.unshift(middleware.outbound);
+		}
+	}
 
-  executeMiddleware(middleware, arg, finish) {
-    function iterator(index) {
-      if (index === middleware.length) {
-        return finish && finish();
-      }
-      middleware[index].call(this, arg, err => {
-        if (err) {
-          return console.log('There was an error: ' + err.message);
-        }
-        iterator.call(this, ++index);
-      });
-    }
+	executeMiddleware(middleware, arg, finish) {
+		function iterator(index) {
+			if (index === middleware.length) {
+				return finish && finish();
+			}
+			middleware[index].call(this, arg, (err) => {
+				if (err) {
+					return console.log("There was an error: " + err.message);
+				}
+				iterator.call(this, ++index);
+			});
+		}
 
-    iterator.call(this, 0);
-  }
+		iterator.call(this, 0);
+	}
 };
