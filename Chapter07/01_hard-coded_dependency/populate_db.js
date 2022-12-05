09bf4d1218ef4eeb3a6321155e9d8b2d6339fd40
1TBhard <was1383@naver.com>
2022-12-05 14:30:34 +0900

    챕터7-1 bcrypt, level 패키지 업데이트

--- Chapter07/01_hard-coded_dependency/populate_db.js
+++ Chapter07/01_hard-coded_dependency/populate_db.js
@@ -1,27 +1,36 @@
 "use strict";
 
-const sublevel = require('level-sublevel');
-const level = require('level');
-const bcrypt = require('bcrypt');
-const uuid = require('node-uuid');
-const async = require('async');
+const sublevel = require("level-sublevel");
+const { Level } = require("level");
+const bcrypt = require("bcrypt");
+const uuid = require("node-uuid");
+const async = require("async");
 
-const db = sublevel(level('example-db', {valueEncoding: 'json'}));
-const usersDb = db.sublevel('users');
+const db = sublevel(Level("example-db", { valueEncoding: "json" }));
+const usersDb = db.sublevel("users");
 
 const users = [
-  {username: 'alice', password: 'secret'},
-  {username: 'bob', password: 'secret'},
-  {username: 'trudy', password: 'secret'}
+	{ username: "alice", password: "secret" },
+	{ username: "bob", password: "secret" },
+	{ username: "trudy", password: "secret" },
 ];
 
-async.forEach(users, (user, callback) => {
-  usersDb.put(user.username, {
-    hash: bcrypt.hashSync(user.password, 8)
-  }, callback);
-}, err => {
-  if(err) {
-    return console.log(err);
-  }
-  console.log('DB populated');
-});
+async.forEach(
+	users,
+	(user, callback) => {
+		usersDb.put(
+			user.username,
+			{
+				hash: bcrypt.hashSync(user.password, 8),
+			},
+			callback
+		);
+	},
+	(err) => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log("DB populated");
+	}
+);
+``;
