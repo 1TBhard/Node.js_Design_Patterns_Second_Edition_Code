2e4d4f94a96335b76c269d26ddbea1133a1bb8d5
1TBhard <was1383@naver.com>
2022-12-06 20:15:14 +0900

    챕터7 - 하드코드된 종속성으로 로그아웃 구현

--- Chapter07/05_plugin_module_impersonation/app.js
+++ Chapter07/05_plugin_module_impersonation/app.js
@@ -1,21 +1,28 @@
 "use strict";
 
-const Express = require('express');
-const bodyParser = require('body-parser');
-const errorHandler = require('errorhandler');
-const http = require('http');
+/* 
+하드코드된 종속성을 사용 
+- requrie()을 사용하여 상대, 절대 경로를 사용하여 어플리케이션의 루트 탐색
+- require() 사용하여 어플리케이션의 모든 서비스에 쉽게 접근
+*/
 
-const authController = require('./lib/authController');
+const Express = require("express");
+const bodyParser = require("body-parser");
+const errorHandler = require("errorhandler");
+const http = require("http");
 
-const app = module.exports = new Express();
+const authController = require("./lib/authController");
+
+const app = (module.exports = new Express());
 app.use(bodyParser.json());
 
-require('authsrv-plugin-logout');
+// 단순히 플러그인을 require()하면 플러그인 실행됨
+require("authsrv-plugin-logout");
 
-app.post('/login', authController.login);
-app.get('/checkToken', authController.checkToken);
+app.post("/login", authController.login);
+app.get("/checkToken", authController.checkToken);
 
 app.use(errorHandler());
 http.createServer(app).listen(3000, () => {
-  console.log('Express server started');
+	console.log("Express server started");
 });
