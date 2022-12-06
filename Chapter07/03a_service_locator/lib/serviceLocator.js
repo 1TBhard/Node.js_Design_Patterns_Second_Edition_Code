98b79ccd4194459d4d1be1f30a476667259138e3
1TBhard <was1383@naver.com>
2022-12-06 20:14:10 +0900

    챕터7 - 서비스 로케이터

--- Chapter07/03a_service_locator/lib/serviceLocator.js
+++ Chapter07/03a_service_locator/lib/serviceLocator.js
@@ -1,28 +1,32 @@
 "use strict";
 
 module.exports = () => {
-  const dependencies = {};
-  const factories = {};
-  const serviceLocator = {};
-  
-  serviceLocator.factory = (name, factory) => {
-    factories[name] = factory;
-  };
-  
-  serviceLocator.register = (name, instance) => {
-    dependencies[name] = instance;
-  };
-  
-  serviceLocator.get = (name) => {
-    if (!dependencies[name]) {
-      const factory = factories[name];
-      dependencies[name] = factory && factory(serviceLocator);
-      if (!dependencies[name]) {
-        throw new Error('Cannot find module: ' + name);
-      }
-    }
-    return dependencies[name];
-  };
+	const dependencies = {};
+	const factories = {};
+	const serviceLocator = {};
 
-  return serviceLocator;
+	// 이름을 해당 팩토리와 연결
+	serviceLocator.factory = (name, factory) => {
+		factories[name] = factory;
+	};
+
+	// 이름을 인스턴스와 직접 연관
+	serviceLocator.register = (name, instance) => {
+		dependencies[name] = instance;
+	};
+
+	// 컴포넌트 검색
+	serviceLocator.get = (name) => {
+		if (!dependencies[name]) {
+			const factory = factories[name];
+
+			dependencies[name] = factory && factory(serviceLocator);
+			if (!dependencies[name]) {
+				throw new Error("Cannot find module: " + name);
+			}
+		}
+		return dependencies[name];
+	};
+
+	return serviceLocator;
 };
