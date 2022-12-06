ead9f3d1ece148efe6d10bffba08b6ceb3162035
1TBhard <was1383@naver.com>
2022-12-06 20:15:50 +0900

    챕터7 - 의존성 컨테이너로 로그아웃 구현

--- Chapter07/08_plugin_di_container/app.js
+++ Chapter07/08_plugin_di_container/app.js
@@ -1,30 +1,38 @@
 "use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const errorHandler = require('errorhandler');
-const http = require('http');
+/* DI 컨테이너
+- 부모 어플리케이션이 어떤 종속성이 필요하는지 몰라도 됨
+- 단점: 각 플러그인이 잠재적으로 모든 서비스에 액세스하여 캡슐화와 통제가 줄어듦
+  - 이를 해결하기 위해 플러그인에 공개하려는 서비스만 등록하는 별도의 DI 컨테이너를 만듦
+*/
 
-const app = module.exports = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const errorHandler = require("errorhandler");
+const http = require("http");
+
+const app = (module.exports = express());
 app.use(bodyParser.json());
 
-const diContainer = require('./lib/diContainer')();
-diContainer.register('dbName', 'example-db');
-diContainer.register('tokenSecret', 'SHHH!');
-diContainer.register('app', app);
-diContainer.factory('db', require('./lib/db'));
-diContainer.factory('authService', require('./lib/authService'));
-diContainer.factory('authController', require('./lib/authController'));
+const diContainer = require("./lib/diContainer")();
 
-//initialize the plugin
-diContainer.inject(require('authsrv-plugin-logout'));
+// di 컨테이너에 의존성 등록
+diContainer.register("dbName", "example-db");
+diContainer.register("tokenSecret", "SHHH!");
+diContainer.register("app", app);
+diContainer.factory("db", require("./lib/db"));
+diContainer.factory("authService", require("./lib/authService"));
+diContainer.factory("authController", require("./lib/authController"));
 
-const authController = diContainer.get('authController');
+// 플러그인 초기화
+diContainer.inject(require("authsrv-plugin-logout"));
 
-app.post('/login', authController.login);
-app.get('/checkToken', authController.checkToken);
+const authController = diContainer.get("authController");
+
+app.post("/login", authController.login);
+app.get("/checkToken", authController.checkToken);
 
 app.use(errorHandler());
 http.createServer(app).listen(3000, () => {
-  console.log('Express server started');
+	console.log("Express server started");
 });
