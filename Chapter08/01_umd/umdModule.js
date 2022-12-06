fd4410ebf0c77f0cc933075b6604e9f8bdb12d8f
1TBhard <was1383@naver.com>
2022-12-06 20:46:25 +0900

    챕터8 - umd 작성

--- Chapter08/01_umd/umdModule.js
+++ Chapter08/01_umd/umdModule.js
@@ -1,22 +1,29 @@
 "use strict";
 
-(function(root, factory) {           //[1]
-  if(typeof define === 'function' && define.amd) {   //[2]
-    define(['mustache'], factory);
-  } else if(typeof module === 'object' &&       //[3]
-    typeof module.exports === 'object') {
-    var mustache = require('mustache');
-    module.exports = factory(mustache);
-  } else {                 //[4]
-    root.UmdModule = factory(root.Mustache);
-  }
-}(this, function(mustache) {           //[5]
-  var template = '<h1>Hello <i>{{name}}</i></h1>';
-  mustache.parse(template);
+// 익명 즉시실행 함수
+// root: 전역 네임스페이스 객체(window)
+(function (root, factory) {
+	// AMD를 현재 시스템에서 사용 가능?
+	if (typeof define === "function" && define.amd) {
+		define(["mustache"], factory);
+	} else if (
+		// CommonJS 환경인지 확인 (node의 export 유무로 확인)
+		typeof module === "object" &&
+		typeof module.exports === "object"
+	) {
+		var mustache = require("mustache");
+		module.exports = factory(mustache);
+	} else {
+		// AMD, CommonJS 가 아닌 경우, UmdModule 로 등록
+		root.UmdModule = factory(root.Mustache);
+	}
+})(this, function (mustache) {
+	var template = "<h1>Hello <i>{{name}}</i></h1>";
+	mustache.parse(template);
 
-  return {
-    sayHello:function(toWhom) {
-      return mustache.render(template, {name: toWhom});
-    }
-  };
-}));
+	return {
+		sayHello: function (toWhom) {
+			return mustache.render(template, { name: toWhom });
+		},
+	};
+});
