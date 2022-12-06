7b79c9b8cf9e861500ae469373ead923bd6c9dfa
1TBhard <was1383@naver.com>
2022-12-06 20:14:19 +0900

    챕터7 - di 컨테이너

--- Chapter07/04_di_container/lib/diContainer.js
+++ Chapter07/04_di_container/lib/diContainer.js
@@ -1,39 +1,43 @@
 "use strict";
 
-const fnArgs = require('parse-fn-args');
+const fnArgs = require("parse-fn-args");
 
 module.exports = () => {
-  const dependencies = {};
-  const factories = {};
-  const diContainer = {};
-  
-  diContainer.factory = (name, factory) => {
-    factories[name] = factory;
-  };
-  
-  diContainer.register = (name, dep) => {
-    dependencies[name] = dep;
-  };
-  
-  diContainer.get = (name) => {
-    if (!dependencies[name]) {
-      const factory = factories[name];
-      dependencies[name] = factory && 
-          diContainer.inject(factory);
-      if (!dependencies[name]) {
-        throw new Error('Cannot find module: ' + name);
-      }
-    }
-    return dependencies[name];
-  };
-  
-  diContainer.inject = (factory) => {
-    const args = fnArgs(factory)
-      .map(function(dependency) {
-        return diContainer.get(dependency);
-      });
-    return factory.apply(null, args);
-  };
-  
-  return diContainer;
+	const dependencies = {};
+	const factories = {};
+	const diContainer = {};
+
+	diContainer.factory = (name, factory) => {
+		factories[name] = factory;
+	};
+
+	diContainer.register = (name, dep) => {
+		dependencies[name] = dep;
+	};
+
+	diContainer.get = (name) => {
+		if (!dependencies[name]) {
+			const factory = factories[name];
+
+			// 로케이터와 다른점
+			dependencies[name] = factory && diContainer.inject(factory);
+			if (!dependencies[name]) {
+				throw new Error("Cannot find module: " + name);
+			}
+		}
+		return dependencies[name];
+	};
+
+	// 로케이터와 다른점
+	diContainer.inject = (factory) => {
+		// 인자들의 목록을 추출하여 인자들의 목록츨 추출
+		const args = fnArgs(factory).map(function (dependency) {
+			return diContainer.get(dependency);
+		});
+
+		// 해당 종속성 인스턴스에 매핑
+		return factory.apply(null, args);
+	};
+
+	return diContainer;
 };
