349e2b52a58ffb3ecc4446e6861f4baf98d67fd1
1TBhard <was1383@naver.com>
2022-12-06 20:15:28 +0900

    챕터7 - 서비스 로케이터로 로그아웃 구현

--- Chapter07/06_plugin_service_locator/lib/serviceLocator.js
+++ Chapter07/06_plugin_service_locator/lib/serviceLocator.js
@@ -1,28 +1,34 @@
 "use strict";
 
+/* 서비스 로케이터
+- 플러그인에 무엇이 필요한지 미리 알필요 없음
+- 모든 컴포넌트를 해당 플러그인에 노출할 때 좋다.
+  - 다른 플러그인에 노출될 수 있다.
+*/
 module.exports = () => {
-  const dependencies = {};
-  const factories = {};
-  const serviceLocator = {};
-  
-  serviceLocator.factory = (name, factory) => {
-    factories[name] = factory;
-  };
-  
-  serviceLocator.register = (name, instance) => {
-    dependencies[name] = instance;
-  };
-  
-  serviceLocator.get = (name) => {
-    if (!dependencies[name]) {
-      const factory = factories[name];
-      dependencies[name] = factory && factory(serviceLocator);
-      if (!dependencies[name]) {
-        throw new Error('Cannot find module: ' + name);
-      }
-    }
-    return dependencies[name];
-  };
+	const dependencies = {};
+	const factories = {};
+	const serviceLocator = {};
 
-  return serviceLocator;
+	serviceLocator.factory = (name, factory) => {
+		factories[name] = factory;
+	};
+
+	serviceLocator.register = (name, instance) => {
+		dependencies[name] = instance;
+	};
+
+	serviceLocator.get = (name) => {
+		if (!dependencies[name]) {
+			const factory = factories[name];
+			dependencies[name] = factory && factory(serviceLocator);
+			if (!dependencies[name]) {
+				throw new Error("Cannot find module: " + name);
+			}
+		}
+		return dependencies[name];
+	};
+
+	// 서비스 로케이터를 반환
+	return serviceLocator;
 };
