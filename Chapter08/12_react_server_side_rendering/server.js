229cffb77346031dbdc916fe4def738269386e31
1TBhard <was1383@naver.com>
2022-12-07 18:33:45 +0900

    8장 - react 서버사이드 렌더링

--- Chapter08/12_react_server_side_rendering/server.js
+++ Chapter08/12_react_server_side_rendering/server.js
@@ -1,38 +1,44 @@
 "use strict";
 
-const http = require('http');
-const Express = require('express');
-const React = require('react');
-const ReactDom = require('react-dom/server');
-const Router = require('react-router');
-const routesConfig = require('./src/routesConfig');
+const http = require("http");
+const Express = require("express");
+const React = require("react");
+const ReactDom = require("react-dom/server");
+const Router = require("react-router");
+const routesConfig = require("./src/routesConfig");
 
 const app = new Express();
 const server = new http.Server(app);
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('*', (req, res) => {
-  Router.match(
-    {routes: routesConfig, location: req.url},
-    (error, redirectLocation, renderProps) => {
-      if (error) {
-        res.status(500).send(error.message)
-      } else if (redirectLocation) {
-        res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-      } else if (renderProps) {
-        let markup = ReactDom.renderToString(<Router.RouterContext {...renderProps} />);
-        res.render('index', {markup});
-      } else {
-        res.status(404).send('Not found')
-      }
-    }
-  );
+// 라우터와 match되는 페이지 보냄
+app.get("*", (req, res) => {
+	Router.match(
+		{ routes: routesConfig, location: req.url },
+		(error, redirectLocation, renderProps) => {
+			if (error) {
+				res.status(500).send(error.message);
+			} else if (redirectLocation) {
+				// 302 리다이렉션, 브라우저가 새 대상으로 이동하게 알림
+				res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+			} else if (renderProps) {
+				// html 코드를 브라우저로 전송
+				let markup = ReactDom.renderToString(
+					<Router.RouterContext {...renderProps} />
+				);
+				res.render("index", { markup });
+			} else {
+				// match되는 것 없는 경우
+				res.status(404).send("Not found");
+			}
+		}
+	);
 });
 
 server.listen(3000, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-  console.info('Server running on http://localhost:3000');
+	if (err) {
+		return console.error(err);
+	}
+	console.info("Server running on http://localhost:3000");
 });
--- Chapter08/12_react_server_side_rendering/webpack.config.js
+++ Chapter08/12_react_server_side_rendering/webpack.config.js
@@ -1,23 +1,23 @@
 "use strict";
 
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry:  path.join(__dirname, "src", "main.js"),
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: path.join(__dirname, "src"),
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: 'babel_cache',
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  }
+	entry: path.join(__dirname, "src", "main.js"),
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		loaders: [
+			{
+				test: path.join(__dirname, "src"),
+				loader: "babel-loader",
+				query: {
+					cacheDirectory: "babel_cache",
+					presets: ["es2015", "react"],
+				},
+			},
+		],
+	},
 };
