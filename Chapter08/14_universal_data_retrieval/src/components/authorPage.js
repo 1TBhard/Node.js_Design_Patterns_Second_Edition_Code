f8c86639866416fe88d7a441beab7294d3ccbe1f
1TBhard <was1383@naver.com>
2022-12-07 18:34:37 +0900

    8장 - 범용 데이터 조회 및 프론트에서 프록시로 CORS 처리

--- Chapter08/14_universal_data_retrieval/src/components/authorPage.js
+++ Chapter08/14_universal_data_retrieval/src/components/authorPage.js
@@ -1,33 +1,36 @@
 "use strict";
 
-const React = require('react');
-const Link = require('react-router').Link;
-const xhrClient = require('../xhrClient');
+const React = require("react");
+const Link = require("react-router").Link;
+const xhrClient = require("../xhrClient");
 
 class AuthorPage extends React.Component {
-  static loadProps(context, cb) {
-    xhrClient.get(`authors/${context.params.id}`)
-      .then(response => {
-        const author = response.data;
-        cb(null, {author});
-      })
-      .catch(error => cb(error))
-    ;
-  }
+	static loadProps(context, cb) {
+		// xhrClient 에 의해 웹 서버, 브라우저인지에 따라 baseURL 이 변경
+		xhrClient
+			.get(`authors/${context.params.id}`)
+			.then((response) => {
+				const author = response.data;
+				cb(null, { author });
+			})
+			.catch((error) => cb(error));
+	}
 
-  render() {
-    return (
-      <div>
-        <h2>{this.props.author.name}'s major works</h2>
-        <ul className="books">{
-          this.props.author.books.map( (book, key) =>
-            <li key={key} className="book">{book}</li>
-          )
-        }</ul>
-        <Link to="/">Go back to index</Link>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<h2>{this.props.author.name}'s major works</h2>
+				<ul className='books'>
+					{this.props.author.books.map((book, key) => (
+						<li key={key} className='book'>
+							{book}
+						</li>
+					))}
+				</ul>
+				<Link to='/'>Go back to index</Link>
+			</div>
+		);
+	}
 }
 
 module.exports = AuthorPage;
--- Chapter08/14_universal_data_retrieval/src/xhrClient.js
+++ Chapter08/14_universal_data_retrieval/src/xhrClient.js
@@ -1,8 +1,10 @@
 "use strict";
 
-const Axios = require('axios');
+const Axios = require("axios");
 
-const baseURL = typeof window !== 'undefined' ? '/api' : 'http://localhost:3001';
-const xhrClient = Axios.create({baseURL});
+// 브라우저 또는 웹 서버에서 코드를 실행중인지 확인
+const baseURL =
+	typeof window !== "undefined" ? "/api" : "http://localhost:3001";
+const xhrClient = Axios.create({ baseURL });
 
 module.exports = xhrClient;
