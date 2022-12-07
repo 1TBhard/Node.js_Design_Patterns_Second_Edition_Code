2f947e2965296459d4633fdc4cdf466dd9f91890
1TBhard <was1383@naver.com>
2022-12-07 14:09:38 +0900

    챕터8 - webpack 버전 1 사용

--- Chapter08/04_webpack_es2015/webpack.config.js
+++ Chapter08/04_webpack_es2015/webpack.config.js
@@ -1,22 +1,23 @@
 "use strict";
 
-const path = require('path');
+const path = require("path");
 
+// webpack 버전 1의 구성요소이다.
 module.exports = {
-  entry:  path.join(__dirname, "src", "main.js"),
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: path.join(__dirname, "src"),
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  }
+	entry: path.join(__dirname, "src", "main.js"),
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		loaders: [
+			{
+				test: path.join(__dirname, "src"),
+				loader: "babel-loader",
+				query: {
+					presets: ["es2015"],
+				},
+			},
+		],
+	},
 };
