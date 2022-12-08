8c305513d00da2af7d44e2fe60d7b4ba874217a9
1TBhard <was1383@naver.com>
2022-12-08 20:42:04 +0900

    9장 - 큐를 이용한 비동기 처리

--- Chapter09/02_async_init_queues/asyncModuleWrapper.js
+++ Chapter09/02_async_init_queues/asyncModuleWrapper.js
@@ -1,49 +1,56 @@
 "use strict";
 
-const asyncModule = require('./asyncModule');
+/* 호출된 모든 작업 요청을 큐에 넣고 초기화
+초기화가 다 되면 큐에서 원래의 비동기 모듈에 위임
 
-//The wrapper
+커맨드, 상태, 프록시를 활용하여 구현함
+*/
+
+const asyncModule = require("./asyncModule");
+
+// The wrapper
 const asyncModuleWrapper = module.exports;
 asyncModuleWrapper.initialized = false;
-asyncModuleWrapper.initialize = function() {
-  activeState.initialize.apply(activeState, arguments);
+asyncModuleWrapper.initialize = function () {
+	activeState.initialize.apply(activeState, arguments);
 };
 
-asyncModuleWrapper.tellMeSomething = function() {
-  activeState.tellMeSomething.apply(activeState, arguments);
+asyncModuleWrapper.tellMeSomething = function () {
+	activeState.tellMeSomething.apply(activeState, arguments);
 };
 
-//The state to use when the module is not yet initialized
+// The state to use when the module is not yet initialized
 let pending = [];
-let notInitializedState = {
 
-  initialize: function(callback) {
-    asyncModule.initialize(function() {
-      asyncModuleWrapper.initalized = true;
-      activeState = initializedState;
-      
-      pending.forEach(function(req) {
-        asyncModule[req.method].apply(null, req.args);
-      });
-      pending = [];
-      
-      callback();
-    });
-  },
-  
-  tellMeSomething: function(callback) {
-    return pending.push({
-      method: 'tellMeSomething',
-      args: arguments
-    });
-  }
-  
+// 커맨드 객체
+let notInitializedState = {
+	initialize: function (callback) {
+		asyncModule.initialize(function () {
+			asyncModuleWrapper.initalized = true;
+			activeState = initializedState;
+
+			pending.forEach(function (req) {
+				// 원래의 매서드 실생
+				asyncModule[req.method].apply(null, req.args);
+			});
+			pending = [];
+
+			callback();
+		});
+	},
+
+	tellMeSomething: function (callback) {
+		// 대기 큐에 삽입
+		return pending.push({
+			// 실행 메서드 지정
+			method: "tellMeSomething",
+			args: arguments,
+		});
+	},
 };
 
-//The state to use when the module is initialized
+// The state to use when the module is initialized
 let initializedState = asyncModule;
 
-
-//Set the initial state to the notInitializedState
+// Set the initial state to the notInitializedState
 let activeState = notInitializedState;
-
